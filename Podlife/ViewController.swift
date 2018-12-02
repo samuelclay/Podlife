c9f81a8a3ae96d418e8a6b0827fe5ed430168191
Samuel Clay <samuel@ofbrooklyn.com>
2018-12-02 14:41:47 -0500

    A mess, starting over.

--- Podlife/ViewController.swift
+++ Podlife/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import SceneKit
 import ARKit
 
-class ViewController: UIViewController, ARSCNViewDelegate {
+class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     var didInitializeScene: Bool = false
@@ -20,20 +20,29 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.didTapScreen))
         self.view.addGestureRecognizer(tapRecognizer)
-        
-        
-//        sceneView.autoenablesDefaultLighting = false;
-        
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
+        // Set the view's delegate
+        sceneView.delegate = self
+//        sceneView.session.delegate = self
+        
+        // Show statistics such as fps and timing information
+        sceneView.showsStatistics = true
+        
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-
         // Run the view's session
-        sceneView.session.run(configuration)
+        configuration.planeDetection = .horizontal
+        sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+//        self.drawScene(position: nil)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -47,43 +56,71 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         if didInitializeScene {
             if let camera = sceneView.session.currentFrame?.camera {
                 var translation = matrix_identity_float4x4
-                translation.columns.3.z = -1.0
+                translation.columns.3.z = -3.0
+                translation.columns.3.x = 3.0
                 let transform = camera.transform * translation
                 let position = SCNVector3(transform.columns.3.x, transform.columns.3.y, transform.columns.3.z)
-                sceneController.addSphere(position: position)
+                self.drawScene(position: position)
             }
         }
     }
 
+    func updatePositionAndOrientationOf(_ node: SCNNode, withPosition position: SCNVector3, relativeTo referenceNode: SCNNode) {
+        let referenceNodeTransform = matrix_float4x4(referenceNode.transform)
+        
+        // Setup a translation matrix with the desired position
+        var translationMatrix = matrix_identity_float4x4
+        translationMatrix.columns.3.x = position.x
+        translationMatrix.columns.3.y = position.y
+        translationMatrix.columns.3.z = position.z
+        
+        // Combine the configured translation matrix with the referenceNode's transform to get the desired position AND orientation
+        let updatedTransform = matrix_multiply(referenceNodeTransform, translationMatrix)
+        node.transform = SCNMatrix4(updatedTransform)
+    }
+    
     // MARK: - ARSCNViewDelegate
     
-    func drawScene() {
-        
-        // Set the view's delegate
-        sceneView.delegate = self
-        
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
+    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+        if !didInitializeScene {
+            if sceneView.session.currentFrame?.camera != nil {
+                didInitializeScene = true
+                self.drawScene(position: nil)
+            }
+        }
+    }
+    
+    func drawScene(position: SCNVector3?) {
         // Create a new scene
-        let scene = SCNScene(named: “art.scnassets/Productivity Pod.scn”)!
+//        let scene = SCNScene(named: "art.scnassets/Podlife v7 bottom.scn")!
+        let scene = SCNScene()
+//        scene.rootNode.scale = SCNVector3(0.1, 0.1, 0.1)
+//        scene.rootNode.position = SCNVector3(x: 20, y: 0, z: -2)
         
+        let boxGeometry = SCNBox(width: 10.0, height: 10.0, length: 10.0, chamferRadius: 1.0)
+        let boxNode = SCNNode(geometry: boxGeometry)
+        scene.rootNode.addChildNode(boxNode)
+        
+        sceneView.autoenablesDefaultLighting = true
+        sceneView.allowsCameraControl = true
         // Set the scene to the view
-        sceneView.scene = scene
+        sceneView.scene = scene[]
         
-        let light = SCNLight()
-        light.type = .omni
-        light.intensity = 2000.0
-        light.color = UIColor(red: 0.8, green: 0.8, blue: 0.8, alpha: 0.8)
+//        updatePositionAndOrientationOf(scene.rootNode, withPosition: position, relativeTo: sceneView.pointOfView!)
+//
+//        let light = SCNLight()
+//        light.type = .omni
+//        light.intensity = 2000.0
+//        light.color = UIColor(red: 0.8, green: 0.8, blue: 0.8, alpha: 0.8)
+//
+//
+//        let lightNode = SCNNode()
+//        lightNode.light = light
+//        lightNode.position = SCNVector3(0, 1, 0)
+//        scene.rootNode.addChildNode(lightNode)
         
-        
-        let lightNode = SCNNode()
-        lightNode.light = light
-        lightNode.position = SCNVector3(0, 1, 0)
-        scene.rootNode.addChildNode(lightNode)
-        
-        let url = Bundle.main.url(forResource: "Podlife v7 bottom", withExtension: "stl")!
-        let node = try! BinarySTLParser.createNodeFromSTL(at: url, unit: .millimeter)
-        scene.rootNode.addChildNode(node)
+//        let url = Bundle.main.url(forResource: "Podlife v7 bottom", withExtension: "stl")!
+//        let node = try! BinarySTLParser.createNodeFromSTL(at: url, unit: .millimeter)
+//        scene.rootNode.addChildNode(node)
     }
 }
